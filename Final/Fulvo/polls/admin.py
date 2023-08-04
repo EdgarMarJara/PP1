384af540541f67ed879a802dd725e42da9c69d31
EdgarMarJara <79977475+EdgarMarJara@users.noreply.github.com>
2023-08-04 18:55:44 -0400

    test numero 1

--- Final/Fulvo/polls/admin.py
+++ Final/Fulvo/polls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Canchas
+from .models import Reserva
+from .models import Gestion 
+
+admin.site.register(Canchas)
+admin.site.register(Reserva)
+admin.site.register(Gestion)
